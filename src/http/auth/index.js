3347b753d8e9eac9d92a8717093252be5c1b742e
Kirill Cherkalov <kirill.cherkalov@effective-soft.com>
2020-12-08 20:29:22 +0300

    update

--- src/http/auth/index.js
+++ src/http/auth/index.js
@@ -3,6 +3,6 @@ import * as formatters from './formatters';
 
 export const login = credentials => http.post('/auth/login', credentials).then(formatters.login);
 
-export const restorePassword = data => http.post('auth/password/forgot', data);
+export const restorePassword = data => http.post('/auth/password/forgot', data);
 
-export const resetPass = data => http.post(`auth/password/reset/?resetPasswordToken=${data.token}`, data);
+export const resetPass = data => http.post(`/auth/password/reset/?resetPasswordToken=${data.token}`, data);
